ace6c25190e3c61fd3f15273cef50ccd80dbdb71
twn2500 <jonathan.r.granberg@gmail.com>
2025-11-13 21:47:37 +0100

    asd

--- Gymnasiearbete/login.js
+++ Gymnasiearbete/login.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh@supabase/supabase-js@2'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 // Supabase-konfiguration
 const supabaseUrl = 'https://ravafoaxjvtxhyduaibu.supabase.co'
