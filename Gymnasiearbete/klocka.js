abc64c0e371667f676abaa8aeaecd22cf01085e3
twn2500 <jonathan.r.granberg@gmail.com>
2026-02-02 09:40:36 +0100

    uasd

--- Gymnasiearbete/klocka.js
+++ Gymnasiearbete/klocka.js
@@ -1,1 +1,30 @@
-const deadline = 'December 24 2025';
+const Dagar = document.getElementById("dagar")
+const Timmar = document.getElementById("timmar")
+const Minuter = document.getElementById("minuter")
+const Sekunder = document.getElementById("sekunder")
+
+const slutDatum = new Date("Juni 12 2026 12:00:00").getTime();
+
+function timer () {
+    
+    const dagensDatum = new Date ().getTime();
+    const distans = slutDatum - dagensDatum;
+
+    const dagar = Math.floor(distans / 1000 / 60 / 60 / 24);
+    const timmar = Math.floor(distans / 1000 / 60 / 60) % 24;
+    const minuter = Math.floor(distans / 1000 / 60) % 60;
+    const sekunder = Math.floor(distans / 1000) % 60;
+
+    Dagar.innerHTML = dagar;
+    Timmar.innerHTML = timmar;
+    Minuter.innerHTML = minuter;
+    Sekunder.innerHTML = sekunder;
+
+    if(distans < 0) {
+        Dagar.innerHTML = "00";
+        Timmar.innerHTML = "00";
+        Minuter.innerHTML = "00";
+        Sekunder.innerHTML = "00";
+    }
+}
+setInterval(timer, 1000)
