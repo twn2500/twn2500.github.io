da461d3fff1d463c66122feabb72389e5a33fb2c
twn2500 <jonathan.r.granberg@gmail.com>
2026-03-24 10:43:02 +0100

    Skrivit html och fixat ett par bilder
    Skalat ner 3 av bilderna så att de passar, Skrev min html för nav hero main. ska lägga till alla bilder och optimera de

--- Gymnasiearbete/kläder.js
+++ Gymnasiearbete/kläder.js
@@ -1,28 +1,24 @@
-// Listor med bildadresser för varje kategori
-const trojor = [
-  'troja1.jpg',
-  'troja2.jpg',
-  'https://unsplash.it/100?random=1'
-];
+console.log("Skriptet är laddat!");
 
-const jeans = [
-  'jeans1.jpg',
-  'jeans2.jpg',
-  'https://unsplash.it/100?random=2'
-];
+const trojor = ["Bilder/troja1.png", "Bilder/troja2.jpg"];
+const jeans = ["Bilder/530205s6.webp"];
+const skor = ["Bilder/daze.webp", "Bilder/jack.jpg" ];
 
-const annat = [
-  'skor1.jpg',
-  'keps1.jpg',
-  'https://unsplash.it/100?random=3'
-];
+const knapp = document.getElementById("slumpa-knapp");
 
-function randomizeOutfit() {
-  // Funktion för att välja en slumpmässig bild från en lista
-  const getRandomImg = (list) => list[Math.floor(Math.random() * list.length)];
 
-  // Uppdatera bilderna i HTML-koden via deras ID
-  document.getElementById('img-troja').src = getRandomImg(trojor);
-  document.getElementById('img-jeans').src = getRandomImg(jeans);
-  document.getElementById('img-annat').src = getRandomImg(annat);
+if (knapp) {
+    knapp.addEventListener("click", function() {
+        console.log("Knappen klickades!");
+
+        const rTroja = trojor[Math.floor(Math.random() * trojor.length)];
+        const rJeans = jeans[Math.floor(Math.random() * jeans.length)];
+        const rSkor = skor[Math.floor(Math.random() * skor.length)];
+
+        document.getElementById("img-troja").src = rTroja;
+        document.getElementById("img-jeans").src = rJeans;
+        document.getElementById("img-skor").src = rSkor;
+    });
+} else {
+    console.error("Kunde inte hitta knappen med id 'slumpa-knapp'");
 }
