4ad309a020892fd140a8d0999c6d1236819f343e
twn2500 <jonathan.r.granberg@gmail.com>
2026-05-05 11:30:28 +0200

    Jag har jobbat med att försöka få en circle att hamna på en random position
    testat med olika metoder på intrenet och sidan tar en jävla tid o ladda

---
+++ JS Spela/spela.js
@@ -0,0 +1,24 @@
+var circle = document.querySelector(".circle")
+circle.style.position = "absolute"
+
+
+
+const getRandomIntegerx = (min, max) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+
+  return Math.floor(Math.random() * (max - min)) + min
+}
+const randomIntegerx = getRandomIntegerx(20, 500)
+
+const getRandomIntegery = (min, max) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+
+  return Math.floor(Math.random() * (max - min)) + min
+}
+const randomIntegery = getRandomIntegery(20, 500)
+
+
+circle.style.top = randomIntegerx + "px"
+circle.style.left = randomIntegery + "px"
